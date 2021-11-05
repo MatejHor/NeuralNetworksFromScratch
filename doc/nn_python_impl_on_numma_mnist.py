5096a9652fd99d45d98cedf92978ffc4cd6aa6f9
MatejHor <mat.horniak@gmail.com>
2021-11-05 12:59:49 +0100

    Update python implementation

--- doc/nn_python_impl_on_numma_mnist.py
+++ doc/nn_python_impl_on_numma_mnist.py
@@ -10,7 +10,7 @@ class DeepNeuralNetwork():
         self.epochs = epochs
         self.l_rate = l_rate
 
-        # we save all parameters in the neural network in this dictionary
+        print("Epochs: {}, LearningRate: {}, Layer: {}".format(epochs, l_rate, sizes))
         self.params = self.initialization()
 
     def sigmoid(self, x, derivative=False):
@@ -19,7 +19,6 @@ class DeepNeuralNetwork():
         return 1/(1 + np.exp(-x))
 
     def softmax(self, x, derivative=False):
-        # Numerically stable with large exponentials
         exps = np.exp(x - x.max())
         if derivative:
             return exps / np.sum(exps, axis=0) * (1 - exps / np.sum(exps, axis=0))
@@ -33,43 +32,26 @@ class DeepNeuralNetwork():
         return np.vectorize(func)(x)
 
     def initialization(self):
-        # number of nodes in each layer
-        input_layer=self.sizes[0]
-        hidden_1=self.sizes[1]
-        hidden_2=self.sizes[2]
-        output_layer=self.sizes[3]
-
-        params = {
-            'W1':np.random.randn(hidden_1, input_layer) * np.sqrt(1. / hidden_1),
-            'W2':np.random.randn(hidden_2, hidden_1) * np.sqrt(1. / hidden_2),
-            'W3':np.random.randn(output_layer, hidden_2) * np.sqrt(1. / output_layer),
-
-            'b1':np.zeros((hidden_1)),
-            'b2':np.zeros((hidden_2)),
-            'b3':np.zeros((output_layer))
-        }
-
+        params = {}
+        layers = self.sizes
+        for index in range(1, len(layers)):
+            params['W' + str(index)] = np.random.randn(layers[index], layers[index - 1]) * np.sqrt(1. / layers[index])
+            params['b' + str(index)] = np.zeros((layers[index]))
         return params
 
     def forward_pass(self, x_train):
         params = self.params
+        len_layers = len(self.sizes)
 
         # input layer activations becomes sample
         params['A0'] = x_train
 
-        # input layer to hidden layer 1
-        params['Z1'] = np.dot(params["W1"], params['A0']) + params['b1']
-        params['A1'] = self.sigmoid(params['Z1'])
+        for index in range(1, len_layers):
+            # input layer to hidden layer 1
+            params['Z' + str(index)] = np.dot(params["W" + str(index)], params['A' + str(index - 1)]) + params['b' + str(index)]
+            params['A' + str(index)] = self.relu(params['Z' + str(index)])
 
-        # hidden layer 1 to hidden layer 2
-        params['Z2'] = np.dot(params["W2"], params['A1']) + params['b2']
-        params['A2'] = self.sigmoid(params['Z2'])
-
-        # hidden layer 2 to output layer
-        params['Z3'] = np.dot(params["W3"], params['A2']) + params['b3']
-        params['A3'] = self.softmax(params['Z3'])
-
-        return params['A3']
+        return params['A' + str(len_layers - 1)]
 
     def backward_pass(self, y_train, output):
         '''
@@ -85,24 +67,17 @@ class DeepNeuralNetwork():
         '''
         params = self.params
         change_w = {}
+        len_layers = len(self.sizes)
 
-        # Calculate W3 update
-        error = 2 * (output - y_train) / output.shape[0] * self.softmax(params['Z3'], derivative=True)
-        change_w['W3'] = np.outer(error, params['A2'])
-        print(change_w['W3'].shape)
-        print(error.shape)
-        change_w['b3'] = np.sum(error, axis=1, keepdims=True)
+        error = 2 * (output - y_train) / output.shape[0] * self.softmax(params['Z' + str(len_layers - 1)], derivative=True)
+        for index in reversed(range(1, len_layers)):
+            # Calculate W3 update
+            change_w['W' + str(index)] = np.outer(error, params['A' + str(index - 1)]) 
+            change_w['b' + str(index)] = error
 
-        # Calculate W2 update
-        error = np.dot(params['W3'].T, error) * self.sigmoid(params['Z2'], derivative=True)
-        change_w['W2'] = np.outer(error, params['A1'])
-        change_w['b1'] = np.sum(error, axis=1, keepdims=True)
-
-        # Calculate W1 update
-        error = np.dot(params['W2'].T, error) * self.sigmoid(params['Z1'], derivative=True)
-        change_w['W1'] = np.outer(error, params['A0'])
-        change_w['b1'] = np.sum(error, axis=1, keepdims=True)
-
+            # Calculate W2 update
+            if index != 1:
+                error = np.dot(params['W' + str(index)].T, error) * self.relu(params['Z' + str(index - 1)], derivative=True)
         return change_w
 
     def update_network_parameters(self, changes_to_w):
@@ -116,7 +91,6 @@ class DeepNeuralNetwork():
                 gradient ∇J(x, y):  the gradient of the objective function,
                                     i.e. the change for a specific theta θ
         '''
-        
         for key, value in changes_to_w.items():
             self.params[key] -= self.l_rate * value
 
@@ -126,14 +100,23 @@ class DeepNeuralNetwork():
             of the maximum value in the output equals the indices in the label
             y. Then it sums over each prediction and calculates the accuracy.
         '''
+        al = []
+
         predictions = []
+        loss = []
 
         for x, y in zip(x_val, y_val):
             output = self.forward_pass(x)
             pred = np.argmax(output)
-            predictions.append(pred == np.argmax(y))
+            predictions.append(pred == np.argmax(y))      
+
+            log = np.log(output)
+            loss.append(y * np.where(log == -np.Inf, 0, log))
+
+            # Output from forward
+            al.append(pred)
         
-        return np.mean(predictions)
+        return np.mean(predictions), (-np.sum(loss)), al
 
     def train(self, x_train, y_train, x_val, y_val):
         start_time = time.time()
@@ -143,41 +126,45 @@ class DeepNeuralNetwork():
                 changes_to_w = self.backward_pass(y, output)
                 self.update_network_parameters(changes_to_w)
             
-            accuracy = self.compute_accuracy(x_val, y_val)
-            print('Epoch: {0}, Time Spent: {1:.2f}s, Accuracy: {2:.2f}%'.format(
-                iteration+1, time.time() - start_time, accuracy * 100
+            accuracy, loss, al = self.compute_accuracy(x_val, y_val)
+            print('Epoch: {0}, Time Spent: {1:.2f}s, Accuracy: {2:.2f}%, Loss: {3:.4f}'.format(
+                iteration+1, time.time() - start_time, accuracy * 100, loss
             ))
             
+# import nn_python_impl_on_numma_mnist as nn
+# # # x, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+# # # x_ = x.to_numpy()
 
 file = open("./data/fashion_mnist_train_vectors.csv")
 x_train = np.loadtxt(file, delimiter=',')
-
 file = open("./data/fashion_mnist_train_labels.csv")
 y_train = np.loadtxt(file, delimiter=',')
 
 file = open("./data/fashion_mnist_test_vectors.csv")
-x_val = np.loadtxt(file, delimiter=',')
+x_test = np.loadtxt(file, delimiter=',')
 file = open("./data/fashion_mnist_test_labels.csv")
-y_val = np.loadtxt(file, delimiter=',')
+y_test = np.loadtxt(file, delimiter=',')
 
-# x, y = fetch_openml('mnist_784', version=1, return_X_y=True)
-# x_ = x.to_numpy()
-x_ = (x_train/255).astype('float64')
-y_ = to_categorical(y_train)
-y_ = y_.astype('float64')
+x_train = (x_train/255).astype('float64')
+y_train = to_categorical(y_train).astype('float64')
 
-x_val = (x_val/255).astype('float64')
-y_val = to_categorical(y_val)
-y_val = y_val.astype('float64')
+x_test = (x_test/255).astype('float64')
+y_test = to_categorical(y_test).astype('float64')
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.164, random_state=42)
 
 print("x_train.shape ", x_train.shape)
 print("y_train.shape ", y_train.shape)
-print("x.shape ", x_.shape)
-print("y.shape ", y_.shape)
-print(y_train[0])
-print(y_[0])
+print("x_val.shape ", x_val.shape)
+print("y_val.shape ", y_val.shape)
+print("x_test.shape ", x_test.shape)
+print("y_test.shape ", y_test.shape)
 
-# x_train, x_val, y_train, y_val = train_test_split(x_, y_, test_size=0.15, random_state=42)
+dnn = DeepNeuralNetwork(sizes=[784, 128, 64, 10], l_rate=0.01, epochs=10)
+dnn.train(x_train, y_train, x_val, y_val)
 
-dnn = DeepNeuralNetwork(sizes=[784, 128, 64, 10])
-dnn.train(x_, y_, x_val, y_val)
+acc, loss, al = dnn.compute_accuracy(x_test, y_test)
+print('--\n', al, '--\n')
+# print('y_test:', y_test)
+
+print("Test Accuracy: {:.2f}%, Loss: {:.4f}".format(acc * 100, loss))
